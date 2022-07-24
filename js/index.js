1134f60f295e6f2a484053d2ea6bdd8ae6fb9d2f
Mariem <benhajalaya_meriam@yahoo.fr>
2022-07-24 13:14:46 +0200

    cart done

--- js/index.js
+++ js/index.js
@@ -30,7 +30,7 @@ let shopItemsData = [ {
     discount:"40%",
     category: "Women",
     name:"Ring",
-    price:"$1500",
+    price:1500,
     image:"./images/d1.jpg"
 }
 , {
@@ -38,24 +38,26 @@ let shopItemsData = [ {
     discount:"40%",
     category: "Women",
     name:"Ring",
-    price:"$1500",
+    price:1500,
     image:"./images/d1.jpg"
 }, {
     id:"3",
     discount:"40%",
     category: "Women",
     name:"Ring",
-    price:"$1500",
+    price:1500,
     image:"./images/d1.jpg"
 }, {
     id:"4",
     discount:"40%",
     category: "Women",
     name:"Ring",
-    price:"$1500",
+    price:1500,
     image:"./images/d1.jpg"
 }]
 
+
+let basket = [];
 
 let generateShop =()=>{
     return (shop.innerHTML=shopItemsData.map(function(x){
@@ -65,9 +67,10 @@ let generateShop =()=>{
               <img src=${image} width="150" alt="" />
               <div>
                 <p>${name}</p>
-                <span>Price: ${price}</span> <br />  
-                <a onclick="increment(${id})"><i class="bx bx-plus"></i></a>
-                <a onclick="decrement(${id})"><i class="bx bx-minus"></i></a>
+                <span>Price: ${price}</span> <br /> 
+                <div id="${id}">0</div>
+                <a onclick="increment(${id},${price})"><i class="bx bx-plus"></i></a>
+                <a onclick="decrement(${id},${price})"><i class="bx bx-minus"></i></a>
                 <a href="#">remove</a>
               </div>
             </div>`
@@ -77,7 +80,62 @@ let generateShop =()=>{
 
 generateShop();
 
-let increment = (id) =>{console.log(id)}
-let decrement = (id) =>{console.log(id)}
-let update = () =>{}
+
+let increment = (id,price) =>{
+    let SelectedItem = id;
+    let search = basket.find((x)=>x.id === SelectedItem);
+
+    if(search == undefined){
+        basket.push({
+            id: SelectedItem,
+            item: 1,
+            itemPrice: price,
+            Total: 0
+        });
+    }
+    else{
+        search.item += 1;
+        search.Total = search.item * search.itemPrice;
+    }
+    
+console.log(basket);
+update(id,search.item);
+};
+
+let decrement = (id) =>{
+    let SelectedItem = id;
+    let search = basket.find((x)=>x.id === SelectedItem);
+
+    if(search.item == 0)
+        return;
+    
+    else{
+        search.item -= 1;
+        search.Total = search.item * search.itemPrice;
+    }
+    
+console.log(basket);
+update(id,search.item);
+};
+
+let update = (id,item) =>{
+    let quantity = document.getElementById(id);
+    quantity.innerHTML = item;
+    let Subtotal = document.getElementById("Subtotal");
+    let T =0;
+    basket.forEach(x => {
+        T += x.Total;
+    } )
+    Subtotal.innerHTML = T;
+    let Tax = T * 20/100 ;
+    document.getElementById("Tax").innerHTML = Tax;
+    document.getElementById("TotalTopay").innerHTML = T + Tax;
+    
+
+    
+    console.log(id);
+}
+
+
+
 
